b307496c11e4cf1ea77ea29bba366277c2158f4c
Mario yaputra <mario34105@gmail.com>
2016-12-05 22:18:36 +0700

    update "delete" function with "unlink" & "rmdir"

--- delete.php
+++ delete.php
@@ -4,10 +4,22 @@
 	$conn = konek_db();
 
 	$id = $_GET["p"];
+	$query1 = $conn->prepare("select * from project where id=?");
+	$query1->bind_param("i",$id);
+	$result1 = $query1->execute();
+
+	$rows = $query1->get_result();
+	$row = $rows->fetch_array();
+	$image = $row["image"];
+	echo $image;
+	unlink("image/projects/".$id."/".$image);
+	rmdir("image/projects/".$id);
 
 	$query = $conn->prepare("delete from project where id=?");
 	$query->bind_param("i",$id);
 	$result = $query->execute();
+
+	
 
 	
  ?>
